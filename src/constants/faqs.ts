1933434509719ee051fe6838c334a42ae20a1ca1
Kuril <chairila.f20@gmail.com>
2026-05-04 09:48:11 +0700

    add: FAQ data

---
+++ src/constants/faqs.ts
@@ -0,0 +1,28 @@
+import type { Faq } from '@/types';
+
+export const FAQS: Faq[] = [
+  {
+    id: 1,
+    question: 'Apakah bisa menggunakan vendor di luar daftar rekanan?',
+    answer:
+      'Sangat bisa. Kami bersifat fleksibel dan akan berkoordinasi dengan vendor pilihan Anda untuk memastikan standar kualitas Kisah Kita tetap terjaga.',
+  },
+  {
+    id: 2,
+    question: 'Berapa lama waktu ideal untuk mulai planning?',
+    answer:
+      'Idealnya 6 hingga 12 bulan sebelumnya. Namun, kami juga pernah menangani Flash Planning dalam waktu 2 bulan untuk acara intimate.',
+  },
+  {
+    id: 3,
+    question: 'Berapa kru yang akan bertugas di hari H?',
+    answer:
+      'Untuk paket management standar, kami menurunkan minimal 10 orang kru (Lead Planner, Runner, LO Pengantin, LO Keluarga, dll) dengan HT aktif.',
+  },
+  {
+    id: 4,
+    question: 'Apakah ada biaya tambahan untuk luar kota?',
+    answer:
+      'Untuk area DIY (Yogyakarta) tidak ada biaya tambahan transport. Di luar DIY, akan ada biaya akomodasi dan transport kru sesuai lokasi.',
+  },
+];
