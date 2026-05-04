d47e851c806690ede40a0525d8caba1d829e4ad4
Kuril <chairila.f20@gmail.com>
2026-05-04 08:53:55 +0700

    add: fake/example business data on constants/business

---
+++ src/constants/business.ts
@@ -0,0 +1,15 @@
+export const BUSINESS_INFO = {
+  name: 'Kisah Kita',
+  tagline: 'Menulis Cerita, Merayakan Rasa',
+  description:
+    'Wedding & Event Planner yang berfokus pada storytelling. Kami tidak hanya mengorganisir acara, tapi menghidupkan filosofi perjalanan cinta Anda dalam setiap detail.',
+  phone: '08985175971',
+  wa: '628985175971',
+  address: 'Jl. Prawirotaman No.45, Kec. Mergangsan',
+  city: 'Yogyakarta',
+  hours: '09.00 - 18.00 (Setiap Hari)',
+  instagram: 'heyitskuril',
+  mapsEmbedSrc:
+    'https://www.google.com/maps/place/Jl.+Prawirotaman+No.45,+Brontokusuman,+Kec.+Mergangsan,+Kota+Yogyakarta,+Daerah+Istimewa+Yogyakarta+55153/@-7.8192701,110.3701352,17z/data=!3m1!4b1!4m6!3m5!1s0x2e7a57a17dbf92bf:0x812da7d9610bf293!8m2!3d-7.8192754!4d110.3727101!16s%2Fg%2F11gxz7n162?entry=ttu&g_ep=EgoyMDI2MDQyOS4wIKXMDSoASAFQAw%3D%3D',
+  founded: '2020',
+} as const;
